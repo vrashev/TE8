a2b261f20ee36c23a783d39d96ca9fef67324abd
vrashev <44356532+vrashev@users.noreply.github.com>
2021-04-19 19:12:50 -0900

    TE8

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  var canvas = createCanvas(600, 400);
+ 
+  // Move the canvas so it’s inside our <div id="sketch-holder">.
+  canvas.parent('sketch-holder');
+
+  background(255, 0, 200);
+}
+
